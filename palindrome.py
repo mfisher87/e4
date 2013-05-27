2aeb144e55d4b8eab358c83db03f8e42edb0b77a
Matt Fisher <mfisher87@gmail.com>
2013-05-27 18:03:33 -0500

    solved

---
+++ palindrome.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3.3
+
+def isPalindromic(i):
+	if int(str(i)[::-1]) == i:
+		return True
+	else: return False
+
+maxPalindromicProduct = 0
+
+for var1 in range(100,999):
+	for var2 in range(100,999):
+		product = var1 * var2
+		if isPalindromic(product):
+			palindromicProduct = product
+			if palindromicProduct > maxPalindromicProduct: 
+				maxPalindromicProduct = palindromicProduct
+				print(palindromicProduct)
+
+print("Hooray!",maxPalindromicProduct)
