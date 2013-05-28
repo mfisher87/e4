37a20087f4bf845dd27928cc1be39b3b940d5b80
Matt Fisher <mfisher87@gmail.com>
2013-05-27 23:07:59 -0500

    variable names

--- palindrome.py
+++ palindrome.py
@@ -3,13 +3,13 @@
 def isPalindromic(i):
 	if int(str(i)[::-1]) == i: return True
 
-maxPalindromicProduct = 0
+palindromicProduct = 0
 
 for var1 in range(100,999):
 	for var2 in range(100,999):
 		product = var1 * var2
-		if product > maxPalindromicProduct and isPalindromic(product): 
-			maxPalindromicProduct = product
-			print(maxPalindromicProduct)
+		if product > palindromicProduct and isPalindromic(product): 
+			palindromicProduct = product
+			print(palindromicProduct)
 
-print("Hooray!",maxPalindromicProduct)
+print("Hooray!",palindromicProduct)
